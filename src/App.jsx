80aad83a390f5ac9031ce7790a6663d6a6b52fe8
MamtaPatil <mamtapatil5252@gmail.com>
2025-04-22 18:06:43 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Student from './pages/Student'
-import Allroutes from './AllRoutes'
-import Navbar from './pages/Navbar'
+import Allroutes from '../src/allRoutes'
 
 function App() {
 
   return (
     <>
-    {/* <Navbar /> */}
      <Allroutes />
     </>
   )
+  
 }
 
 export default App
